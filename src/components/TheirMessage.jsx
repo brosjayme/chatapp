333af9257421501ee5a786fbcb25f63f33419a90
unknown <brosjayme@gmail.com>
2023-08-23 21:37:44 +0100

    added message components

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
-const TheirMessage = () => {
-  return <div>Their Message</div>;
+const TheirMessage = (lastMessage, message) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{
+            backgroundImage: message.sender && `url(${message.sender.avatar})`,
+          }}
+        />
+      )}
+
+      {message.attachments && message.attachments.length > 0 ? (
+        <img
+          src="{message.attachment[0].file}"
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#cabcdc",
+            marginLeft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default TheirMessage;
